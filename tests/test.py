194fa7003664413d1e1cf4b39a483d59c0d312ab
aviram613i <aviram613i@gmail.com>
2025-01-31 18:20:35 +0200

    refactor tests

--- tests/test.py
+++ tests/test.py
@@ -1,55 +1,100 @@
 import unittest
-from typing import Callable, Dict
+from typing import Callable, List
 
 import pandas as pd
-from pandas._testing import assert_frame_equal
 
-from aggregations import get_avg_subsets, get_count_subsets, get_count_distinct_subsets, get_max_subsets, \
-    get_min_subsets, get_sum_subsets
+from aggregations import get_avg_subsets, get_count_distinct_subsets, get_max_subsets, \
+    get_min_subsets, get_sum_subsets, get_count_subsets
 from optimal_subset_with_constraint import get_optimal_subset
+
+GROUP_COLS = ['grouping_1', 'grouping_2']
+AGG_COL = 'aggregator'
+
+
+def get_min(df: pd.DataFrame) -> pd.Series:
+    return df.min()
+
+
+def get_max(df: pd.DataFrame) -> pd.Series:
+    return df.max()
+
+
+def get_count(df: pd.DataFrame) -> pd.Series:
+    return df.count()
+
+
+def get_count_distinct(df: pd.DataFrame) -> pd.Series:
+    return df.nunique()
+
+
+def get_sum(df: pd.DataFrame) -> pd.Series:
+    return df.sum()
+
+
+def get_avg(df: pd.DataFrame) -> pd.Series:
+    return df.mean()
+
+
+TEST_PARAMS = {
+    'max': [get_max_subsets, get_max],
+    'min': [get_min_subsets, get_min],
+    'count': [get_count_subsets, get_count],
+    'count_distinct': [get_count_distinct_subsets, get_count_distinct],
+    'sum': [get_sum_subsets, get_sum],
+    'avg': [get_avg_subsets, get_avg]
+}
+
+
+def check_trend(
+        df: pd.DataFrame, group_cols: List[str], agg_col: str, agg: Callable[[pd.DataFrame], pd.Series]
+) -> bool:
+    agg_df = agg(
+        df[group_cols + [agg_col]]
+        .groupby(group_cols)
+    )[agg_col].to_frame()
+    sorted_df = agg_df.sort_values(by=agg_col)
+    return agg_df.equals(sorted_df)
+
+
+def test_agg(agg_name: str):
+    subset_agg_func, agg_func = TEST_PARAMS[agg_name]
+
+    input_file_name = f'{agg_name}/input.csv'
+    input_df = pd.read_csv(input_file_name)
+    expected_file_name = f'{agg_name}/expected.csv'
+    expected_df = pd.read_csv(expected_file_name)
+
+    result_df, removed_df = get_optimal_subset(input_df, GROUP_COLS, AGG_COL, subset_agg_func)
+
+    if not check_trend(result_df, GROUP_COLS, AGG_COL, agg_func):
+        raise Exception('Expected does not satisfy trend')
+    if not check_trend(result_df, GROUP_COLS, AGG_COL, agg_func):
+        raise Exception('Solution does not satisfy trend')
+
+    if len(result_df) > len(expected_df):
+        raise Exception('Solution bigger than expected')
+    if len(result_df) < len(expected_df):
+        raise Exception('Solution smaller than expected')
 
 
 class TestOptimalSolution(unittest.TestCase):
     def test_max(self):
-        actual_solution = get_optimal_solution("max/max_initial_file.csv", get_max_subsets)
-        expected_solution = pd.read_csv("max/max_expected_result.csv")
-        assert_data_frames_equal(actual_solution, expected_solution)
+        test_agg('max')
 
     def test_min(self):
-        actual_solution = get_optimal_solution("min/min_initial_file.csv", get_min_subsets)
-        expected_solution = pd.read_csv("min/min_expected_result.csv")
-        assert_data_frames_equal(actual_solution, expected_solution)
+        test_agg('min')
 
     def test_count(self):
-        actual_solution = get_optimal_solution("count/count_initial_file.csv", get_count_subsets)
-        expected_solution = pd.read_csv("count/count_expected_result.csv")
-        assert_data_frames_equal(actual_solution, expected_solution)
+        test_agg('count')
 
     def test_count_distinct(self):
-        actual_solution = get_optimal_solution("count_distinct/count_distinct_initial_file.csv",
-                                               get_count_distinct_subsets)
-        expected_solution = pd.read_csv("count_distinct/count_distinct_expected_result.csv")
-        assert_data_frames_equal(actual_solution, expected_solution)
+        test_agg('count_distinct')
 
     def test_sum(self):
-        actual_solution = get_optimal_solution("sum/sum_initial_file.csv", get_sum_subsets)
-        expected_solution = pd.read_csv("sum/sum_expected_result.csv")
-        assert_data_frames_equal(actual_solution, expected_solution)
+        test_agg('sum')
 
-    def test_average(self):
-        actual_solution = get_optimal_solution("average/average_initial_file.csv", get_avg_subsets)
-        expected_solution = pd.read_csv("average/average_expected_result.csv")
-        assert_data_frames_equal(actual_solution, expected_solution)
-
-
-def get_optimal_solution(input_file_name: str, agg: Callable[[pd.DataFrame, str], Dict[float, set]]) -> pd.DataFrame:
-    df = pd.read_csv(input_file_name)
-    result_df, removed_df = get_optimal_subset(df, ['grouping_1', 'grouping_2'], 'aggregator', agg)
-    return result_df
-
-
-def assert_data_frames_equal(df1: pd.DataFrame, df2: pd.DataFrame):
-    assert_frame_equal(df1.reset_index(drop=True), df2.reset_index(drop=True), False)
+    def test_avg(self):
+        test_agg('avg')
 
 
 if __name__ == '__main__':
