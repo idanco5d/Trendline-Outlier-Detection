06f3aa8ce917abcd469aa6a02f94f7307d666b03
Shunit Agmon <shunit.agmon@gmail.com>
2025-03-09 12:06:44 +0200

    bugfix in median even subsets. minor bugfix in main.

--- aggregations.py
+++ aggregations.py
@@ -2,6 +2,8 @@ from itertools import combinations
 from typing import Dict
 from tqdm import tqdm
 import pandas as pd
+import numpy as np
+error_epsilon = 0.00001
 
 
 def get_index_set(df: pd.DataFrame) -> set:
@@ -116,25 +118,76 @@ def _get_median_subset_odd(df: pd.DataFrame, agg_col: str, median: float) -> set
     return get_index_set(subset_df)
 
 
+#def _get_median_subset_even(df: pd.DataFrame, agg_col: str, low: float, high: float) -> set[int]:
+#    median = (low + high) / 2
+#    sorted_df = df.sort_values(by=agg_col, inplace=False)
+#    smaller_df = df.loc[df[agg_col].le(low)]
+#    greater_df = df.loc[df[agg_col].ge(high)]
+#
+#    if len(smaller_df) < len(greater_df):
+#        subset_df = pd.concat([
+#            smaller_df,
+#            greater_df.head(len(smaller_df))
+#        ])
+#    elif len(smaller_df) > len(greater_df):
+#        subset_df = pd.concat([
+#            smaller_df.tail(len(greater_df)),
+#            greater_df
+#        ])
+#    else:
+#        subset_df = df
+#
+#    if subset_df[agg_col].median() != median:
+#        raise Exception('Reached wrong median')
+#
+#    return get_index_set(subset_df)
+    
 def _get_median_subset_even(df: pd.DataFrame, agg_col: str, low: float, high: float) -> set[int]:
     median = (low + high) / 2
-    smaller_df = df.loc[df[agg_col].le(low)]
-    greater_df = df.loc[df[agg_col].ge(high)]
+    smaller_df = df.loc[df[agg_col].lt(low)]
+    eq_to_low = df.loc[df[agg_col].eq(low)]
+    low_instance = eq_to_low.iloc[[0]]
+    eq_to_low = eq_to_low.iloc[1:]
+    
+    greater_df = df.loc[df[agg_col].gt(high)]
+    eq_to_high = df.loc[df[agg_col].eq(high)]
+    high_instance = eq_to_high.iloc[[0]]
+    eq_to_high = eq_to_high.iloc[1:]
+    
+    left = len(smaller_df) + len(eq_to_low)
+    right = len(greater_df) + len(eq_to_high)
 
-    if len(smaller_df) < len(greater_df):
+    if left < right:
         subset_df = pd.concat([
             smaller_df,
-            greater_df.head(len(smaller_df))
+            eq_to_low,
+            low_instance,
+            high_instance,
+            pd.concat([eq_to_high, greater_df]).head(left)
         ])
-    elif len(smaller_df) > len(greater_df):
+    elif left > right:
         subset_df = pd.concat([
-            smaller_df.tail(len(greater_df)),
+            pd.concat([smaller_df, eq_to_low]).tail(right),
+            low_instance,
+            high_instance,
+            eq_to_high,
             greater_df
         ])
     else:
-        subset_df = df
+        subset_df = pd.concat([
+            smaller_df, 
+            eq_to_low,
+            low_instance,
+            high_instance,
+            eq_to_high,
+            greater_df
+        ])
 
-    if subset_df[agg_col].median() != median:
+    if np.abs(subset_df[agg_col].median() - median) > error_epsilon:
+        print(subset_df[agg_col].values)
+        print(len(subset_df))
+        print(subset_df[agg_col].median())
+        print(median)
         raise Exception('Reached wrong median')
 
     return get_index_set(subset_df)
