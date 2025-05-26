d558d23fced45b9490b349624c4b66c23a4c0fbb
shunita <shunit.agmon@gmail.com>
2025-05-26 23:20:37 +0300

    control aggpack optimization with a command line flag

--- input_parser.py
+++ input_parser.py
@@ -11,7 +11,7 @@ from aggregations import get_avg_subsets, get_count_subsets, get_count_distinct_
 from aggregations_pruning import get_sum_subsets_pruning, get_avg_subsets_pruning, get_median_subsets_pruning, AggregationPruningFunction
 
 from aggregations_mem import AggregationMem, SumAggregation, SumAggregationOpt, AvgAggregation, AvgAggregationPruning, \
-    MedianAggregationOpt
+    MedianAggregationOpt, MedianAggregation, MaxAggregation
 
 AGGREGATIONS = {
     'AVG': get_avg_subsets,
@@ -37,7 +37,13 @@ MEM_AGGREGATIONS = {
 
 MEM_AND_PRUNING_AGGREGATIONS = {
     'AVG': AvgAggregationPruning,
-    'SUM': SumAggregationOpt,  # TODO: it's not using the pruning but it's already fast. We could still use pruning and see if it helps.
+    'SUM': SumAggregation,
+    'MEDIAN': MedianAggregation,
+    'MAX': MaxAggregation,  # Agg pack pruning not available
+}
+
+OPTIMIZED_AGGREGATIONS = {
+    'SUM': SumAggregationOpt,  # agg pack pruning not available
     'MEDIAN': MedianAggregationOpt,
 }
 
@@ -52,6 +58,7 @@ class Input:
     orig_fname: str
     agg_name: str
     mem_opt: bool = False
+    agg_pack_opt: bool = False
     prune_h: bool = None
     prune_aggpack_by_greedy: int = False
     prune_dp_by_greedy: int = False
@@ -71,9 +78,13 @@ def parse_input() -> Input:
     prune_h = args.prune_h
     mem_opt = args.mem_opt
     agg_name = args.aggregation_function
-    aggregation = get_aggregation_function(args.aggregation_function, is_pruning=prune is not None, is_mem_opt=mem_opt)
+    aggregation = get_aggregation_function(args.aggregation_function, args.agg_pack_opt)
+    if (args.aggregation_function == 'SUM' and args.agg_pack_opt and prune_aggpack_by_greedy):
+        raise Exception("Unsupported combination: sum, agg_pack_opt, prune_aggpack_by_greedy")
+    elif (args.aggregation_function == 'MAX' and (args.agg_pack_opt or prune_aggpack_by_greedy)):
+        raise Exception(f"Unsupported combination: max has no aggpack optimizations. agg_pack_opt: {args.agg_pack_opt}, by greedy: {prune_aggpack_by_greedy}")
     time_cutoff_seconds = args.cutoff_seconds
-    return Input(df=df, group_cols=group_cols, agg_col=agg_col, aggregation=aggregation, prune=prune,
+    return Input(df=df, group_cols=group_cols, agg_col=agg_col, aggregation=aggregation,
                  mem_opt=mem_opt,
                  prune_h=prune_h, prune_dp_by_greedy=prune_dp_by_greedy, prune_aggpack_by_greedy=prune_aggpack_by_greedy,
                  time_cutoff_seconds=time_cutoff_seconds,
@@ -96,6 +107,8 @@ def get_input_arguments() -> argparse.Namespace:
     parser.set_defaults(prune_h=False)
     parser.add_argument('--mem_opt', action=argparse.BooleanOptionalAction)
     parser.set_defaults(mem_opt=False)
+    parser.add_argument('--agg_pack_opt', action=argparse.BooleanOptionalAction)
+    parser.set_defaults(agg_pack_opt=False)
     parser.add_argument('--cutoff_seconds', type=int, metavar='N', help='time cutoff in seconds')
     parser.add_argument('--output_folder', type=str, help='path to save results', required=True)
 
@@ -123,24 +136,35 @@ def group_frame_by_attributes(df: pd.DataFrame, grouping_cols: List[str], agg_co
     return df_grouped
 
 
-def get_aggregation_function(
-        function_name: str, is_pruning: bool = False, is_mem_opt: bool = False
-) -> Union[AggregationFunction, AggregationPruningFunction]:
-    if is_pruning and is_mem_opt:
-        if function_name not in MEM_AND_PRUNING_AGGREGATIONS.keys():
-            raise ValueError(f'Unrecognized aggregation function for pruning with mem opt: {function_name}')
-        return MEM_AND_PRUNING_AGGREGATIONS[function_name]
+def get_aggregation_function(function_name: str, agg_pack_opt: bool) -> AggregationMem:
+    if agg_pack_opt:
+        if function_name not in OPTIMIZED_AGGREGATIONS:
+            raise ValueError(f'Unrecognized aggregation function for optimized aggregation: {function_name}')
+        return OPTIMIZED_AGGREGATIONS[function_name]
+    # Otherwise - naive agg packing
+    if function_name not in MEM_AND_PRUNING_AGGREGATIONS.keys():
+        raise ValueError(f'Unrecognized aggregation function for pruning with mem opt: {function_name}')
+    return MEM_AND_PRUNING_AGGREGATIONS[function_name]
 
-    if is_pruning:
-        if function_name not in PRUNING_AGGREGATIONS.keys():
-            raise ValueError(f'Unrecognized aggregation function for pruning: {function_name}')
-        return PRUNING_AGGREGATIONS[function_name]
 
-    if is_mem_opt:
-        if function_name not in MEM_AGGREGATIONS.keys():
-            raise ValueError(f'Unrecognized aggregation function for mem optimization: {function_name}')
-        return MEM_AGGREGATIONS[function_name]
-
-    if function_name not in AGGREGATIONS.keys():
-        raise ValueError(f'Unrecognized aggregation function: {function_name}')
-    return AGGREGATIONS[function_name]
+# def get_aggregation_function(
+#         function_name: str, is_pruning: bool = False, is_mem_opt: bool = False
+# ) -> Union[AggregationFunction, AggregationPruningFunction]:
+#     if is_pruning and is_mem_opt:
+#         if function_name not in MEM_AND_PRUNING_AGGREGATIONS.keys():
+#             raise ValueError(f'Unrecognized aggregation function for pruning with mem opt: {function_name}')
+#         return MEM_AND_PRUNING_AGGREGATIONS[function_name]
+#
+#     if is_pruning:
+#         if function_name not in PRUNING_AGGREGATIONS.keys():
+#             raise ValueError(f'Unrecognized aggregation function for pruning: {function_name}')
+#         return PRUNING_AGGREGATIONS[function_name]
+#
+#     if is_mem_opt:
+#         if function_name not in MEM_AGGREGATIONS.keys():
+#             raise ValueError(f'Unrecognized aggregation function for mem optimization: {function_name}')
+#         return MEM_AGGREGATIONS[function_name]
+#
+#     if function_name not in AGGREGATIONS.keys():
+#         raise ValueError(f'Unrecognized aggregation function: {function_name}')
+#     return AGGREGATIONS[function_name]
--- main.py
+++ main.py
@@ -9,55 +9,66 @@ if __name__ == '__main__':
     s = time.time()
     input_data = parse_input()
     print("The parsed data is: \n", input_data.df)
-    
-    if input_data.prune is not None and input_data.mem_opt:
-        if input_data.agg_name != 'MEDIAN':
-            subset_df, removed_df = get_optimal_subset_pruning_mem_opt(
-                df=input_data.df,
-                group_cols=input_data.group_cols,
-                agg_col=input_data.agg_col,
-                Agg=input_data.aggregation,
-                max_removed=input_data.prune,
-                time_cutoff_seconds=input_data.time_cutoff_seconds,
-                parallelize=True,
-            )
-        else:
-            subset_df, removed_df = get_optimal_subset_F_first(
-                df=input_data.df,
-                group_cols=input_data.group_cols,
-                agg_col=input_data.agg_col,
-                Agg=input_data.aggregation,
-                # time_cutoff_seconds=input_data.time_cutoff_seconds,
-                max_removed=input_data.prune_aggpack_by_greedy,
-                prune_dp_by_max_removed=input_data.prune_dp_by_greedy,
-                prune_h=input_data.prune_h
-            )
 
-    elif input_data.prune is not None:
-        subset_df, removed_df = get_optimal_subset_pruning(
-            df=input_data.df,
-            group_cols=input_data.group_cols,
-            agg_col=input_data.agg_col,
-            agg=input_data.aggregation,
-            max_removed=input_data.prune,
-            parallelize=False,
-        )
-    elif input_data.mem_opt:
-        #subset_df, removed_df = get_optimal_subset_mem_opt(
-        subset_df, removed_df = get_optimal_subset_F_first(
-            df=input_data.df,
-            group_cols=input_data.group_cols,
-            agg_col=input_data.agg_col,
-            Agg=input_data.aggregation,
-            #time_cutoff_seconds=input_data.time_cutoff_seconds,
-        )
-    else:
-        subset_df, removed_df = get_optimal_subset(
-            df=input_data.df,
-            group_cols=input_data.group_cols,
-            agg_col=input_data.agg_col,
-            agg=input_data.aggregation
-        )
+    subset_df, removed_df = get_optimal_subset_F_first(
+        df=input_data.df,
+        group_cols=input_data.group_cols,
+        agg_col=input_data.agg_col,
+        Agg=input_data.aggregation,
+        # time_cutoff_seconds=input_data.time_cutoff_seconds,
+        max_removed=input_data.prune_aggpack_by_greedy,
+        prune_dp_by_max_removed=input_data.prune_dp_by_greedy,
+        prune_h=input_data.prune_h
+    )
+
+    # if input_data.prune_aggpack_by_greedy is not None and input_data.mem_opt:
+    #     if input_data.agg_name != 'MEDIAN':
+    #         subset_df, removed_df = get_optimal_subset_pruning_mem_opt(
+    #             df=input_data.df,
+    #             group_cols=input_data.group_cols,
+    #             agg_col=input_data.agg_col,
+    #             Agg=input_data.aggregation,
+    #             max_removed=input_data.prune,
+    #             time_cutoff_seconds=input_data.time_cutoff_seconds,
+    #             parallelize=True,
+    #         )
+    #     else:
+    #         subset_df, removed_df = get_optimal_subset_F_first(
+    #             df=input_data.df,
+    #             group_cols=input_data.group_cols,
+    #             agg_col=input_data.agg_col,
+    #             Agg=input_data.aggregation,
+    #             # time_cutoff_seconds=input_data.time_cutoff_seconds,
+    #             max_removed=input_data.prune_aggpack_by_greedy,
+    #             prune_dp_by_max_removed=input_data.prune_dp_by_greedy,
+    #             prune_h=input_data.prune_h
+    #         )
+    #
+    # elif input_data.prune is not None:
+    #     subset_df, removed_df = get_optimal_subset_pruning(
+    #         df=input_data.df,
+    #         group_cols=input_data.group_cols,
+    #         agg_col=input_data.agg_col,
+    #         agg=input_data.aggregation,
+    #         max_removed=input_data.prune,
+    #         parallelize=False,
+    #     )
+    # elif input_data.mem_opt:
+    #     #subset_df, removed_df = get_optimal_subset_mem_opt(
+    #     subset_df, removed_df = get_optimal_subset_F_first(
+    #         df=input_data.df,
+    #         group_cols=input_data.group_cols,
+    #         agg_col=input_data.agg_col,
+    #         Agg=input_data.aggregation,
+    #         #time_cutoff_seconds=input_data.time_cutoff_seconds,
+    #     )
+    # else:
+    #     subset_df, removed_df = get_optimal_subset(
+    #         df=input_data.df,
+    #         group_cols=input_data.group_cols,
+    #         agg_col=input_data.agg_col,
+    #         agg=input_data.aggregation
+    #     )
 
     print(f"Num removed tuples: {len(removed_df)}/{len(input_data.df)}")
 
