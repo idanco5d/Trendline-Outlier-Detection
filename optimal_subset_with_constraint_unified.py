5940fafa0b11bb5d0745e31aaaa218bd090df8ea
shunita <shunit.agmon@gmail.com>
2025-06-08 12:50:30 +0300

    rename for clarity

--- optimal_subset_with_constraint_unified.py
+++ optimal_subset_with_constraint_unified.py
@@ -57,7 +57,7 @@ def update_H_no_pruning(F, H, group_id):
     return H
 
 
-def prune_H(H, max_removed=None, sum_of_groups=None):
+def prune_H(H, max_removed=None, sum_of_group_sizes=None):
     """
     If x1<=x2 and H(x1)>=H(x2), keep only x1, and prune x2.
     :param H: sorted dict of agg value (x) to: (count, [(agg_value, group_id)]) for groups 1..i, such that agg value of group i = x)
@@ -67,7 +67,7 @@ def prune_H(H, max_removed=None, sum_of_groups=None):
     for option in H.keys():
         if max_removed is not None:
             # compute removal from groups 1,.., i. If it's too large, no need to remember this option.
-            if (sum_of_groups - H[option][0]) > max_removed:
+            if (sum_of_group_sizes - H[option][0]) > max_removed:
                 continue
         if H[option][0] > max_count:
             newH[option] = H[option]
@@ -75,7 +75,7 @@ def prune_H(H, max_removed=None, sum_of_groups=None):
     return SortedDict(newH)
 
 
-def prune_H_by_max_removed(H, max_removed, sum_of_groups=None):
+def prune_H_by_max_removed(H, max_removed, sum_of_group_sizes=None):
     """
     If x1<=x2 and H(x1)>=H(x2), keep only x1, and prune x2.
     :param H: sorted dict of agg value (x) to: (count, [(agg_value, group_id)]) for groups 1..i, such that agg value of group i = x)
@@ -83,7 +83,7 @@ def prune_H_by_max_removed(H, max_removed, sum_of_groups=None):
     newH = {}
     for option in H.keys():
         # compute removal from groups 1,.., i-1. If it's too large, no need to remember this option.
-        if (sum_of_groups - H[option][0]) > max_removed:
+        if (sum_of_group_sizes - H[option][0]) > max_removed:
             continue
         newH[option] = H[option]
     return SortedDict(newH)
@@ -128,17 +128,17 @@ def get_optimal_subset_F_first(
         group_keys.append(group_key)
         group_sizes[group_key] = len(group_df)
     # Next, compute the solution (main DP).
-    size_of_groups = 0
+    sum_of_group_sizes = 0
     for group_key in group_keys:
         print(f"merging + pruning group: {group_key}")
-        size_of_groups += group_sizes[group_key]
+        sum_of_group_sizes += group_sizes[group_key]
         if prune_h:
             H = update_H_with_pruning(output[group_key], H, group_key)
-            H = prune_H(H, prune_dp_by_max_removed, size_of_groups) #TODO: there is a bug here when prune_dp_by_max_removed isn't None!
+            H = prune_H(H, prune_dp_by_max_removed, sum_of_group_sizes) #TODO: there is a bug here when prune_dp_by_max_removed isn't None!
         else:
             H = update_H_no_pruning(output[group_key], H, group_key)
             if prune_dp_by_max_removed is not None:
-                H = prune_H_by_max_removed(H, prune_dp_by_max_removed, size_of_groups)
+                H = prune_H_by_max_removed(H, prune_dp_by_max_removed, sum_of_group_sizes)
 
     ids_to_keep = []
     if prune_h:
