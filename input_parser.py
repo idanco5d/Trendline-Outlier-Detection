f6e068230a130b5e091a742e1620b009cde78134
Carmel Paradis <16carmel@gmail.com>
2025-08-30 18:20:25 +0300

    'data' field

--- input_parser.py
+++ input_parser.py
@@ -53,16 +53,17 @@ OPTIMIZED_AGGREGATIONS = {
 @dataclass
 class RawArgs:
     aggregation_function: str
-    dataset_file_name: str
     aggregation_column: str
     grouping_columns: list[str]
     output_folder: str
-    prune_aggpack_by_greedy: Optional[int] = None
-    prune_dp_by_greedy: Optional[int] = None
-    prune_h: Optional[bool] = None
-    mem_opt: Optional[bool] = None
-    agg_pack_opt: Optional[bool] = None
-    cutoff_seconds: Optional[int] = None
+    dataset_file_name: str | None = None
+    data: dict | None = None
+    prune_aggpack_by_greedy: int | None = None
+    prune_dp_by_greedy: int | None = None
+    prune_h: bool | None = None
+    mem_opt: bool | None = None
+    agg_pack_opt: bool | None = None
+    cutoff_seconds: int | None = None
 
 
 @dataclass
