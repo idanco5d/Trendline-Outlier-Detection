c67f9f6ee66769ed400a22d3b1233b9b9a6915c8
Carmel Paradis <16carmel@gmail.com>
2025-09-19 11:15:09 +0300

    use background version as well (in the api)

--- api.py
+++ api.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-
 from input_parser import RawArgs
-from main import run_algorithm
+from algorithm import run_algorithm, run_algorithm_background
 
 app = Flask(__name__)
 CORS(app)
@@ -11,11 +10,18 @@ CORS(app)
 @app.route("/algorithm", methods=['POST'])
 def run_algorithm_route():
     try:
-        request_args = RawArgs(**request.json)
-        _, subset_df, removed_df = run_algorithm(initial_args=request_args)
-        return jsonify({
-            'subset_df': subset_df.to_dict('records'),
-            'removed_df': removed_df.to_dict('records')
-        }), 200
+        background = request.args.get('background', False)
+        if not background:
+            request_args = RawArgs(**request.json)
+            _, subset_df, removed_df = run_algorithm(initial_args=request_args)
+            return jsonify({
+                'subset_df': subset_df.to_dict('records'),
+                'removed_df': removed_df.to_dict('records')
+            }), 200
+        else:
+            task = run_algorithm_background.delay(request.json)
+            return jsonify({
+                'task_id': task.id,
+            }), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
