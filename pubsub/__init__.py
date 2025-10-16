c2bb0a2ee41775cc942488cf5ec3a39ea1817c65
Carmel Paradis <16carmel@gmail.com>
2025-10-16 22:28:22 +0300

    pubsub module

--- pubsub/__init__.py
+++ pubsub/__init__.py
@@ -1,1 +1,2 @@
-from pubsub.redis_pubsub import with_redis_pubsub, get_pubsub_topic, get_redis_pubsub_async_client
+from pubsub.redis_pubsub import get_pubsub_topic, get_redis_pubsub_async_client, \
+    get_redis_sync_client, redis_sync_client_cleanup
--- pubsub/redis_pubsub.py
+++ pubsub/redis_pubsub.py
@@ -1,5 +1,3 @@
-import functools
-import json
 import os
 import ssl
 
@@ -12,21 +10,12 @@ async def get_redis_pubsub_async_client() -> async_redis.Redis:
     # Use from_url to initialize the async client
     return async_redis.Redis.from_url(os.environ['REDIS_URL'], ssl_cert_reqs=ssl.CERT_NONE)
 
-def _get_redis_sync_client() -> redis.Redis:
+def get_redis_sync_client() -> redis.Redis:
     return redis.Redis.from_url(os.environ['REDIS_URL'], ssl_cert_reqs=ssl.CERT_NONE)
 
-def _redis_sync_client_cleanup(client: redis.Redis):
+def redis_sync_client_cleanup(client: redis.Redis):
     if client and client.connection_pool:
         client.connection_pool.disconnect()
-
-def with_redis_pubsub(func):
-    @functools.wraps(func)
-    def wrapper(topic: str, *args, **kwargs):
-        client = _get_redis_sync_client()
-        data = func(*args, **kwargs)
-        client.publish(topic, json.dumps(data))
-        _redis_sync_client_cleanup(client)
-    return wrapper
 
 def get_pubsub_topic(task_id: str) -> str:
     return f'task:{task_id}'
